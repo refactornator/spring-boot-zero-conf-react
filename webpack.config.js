671e196219924c458ad67d6d2bd112158af596b6
William Lindner <wlindner522@gmail.com>
2019-09-28 21:51:23 -0700

    Use webpack to create the html template for thymeleaf
    - Webpack can add a hash or add additional static resources to the HTML file during bundling
    - Thymeleaf can add additional variables later, if needed
    - Add spring-boot-devtools for frontend reloads

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const resources = path.resolve(__dirname, 'src/main/resources')
 
 module.exports = {
   mode: 'development',
   entry: './frontend/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'src/main/resources/static'),
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.join(resources, 'static'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './frontend/index.html',
+      filename: path.join(resources, 'templates', 'index.html')
+    })
+  ]
 };
