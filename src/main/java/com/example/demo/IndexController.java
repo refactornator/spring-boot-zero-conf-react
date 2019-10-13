cc5e76f7891c6837d18fc6796f8d7800718070af
William Lindner <wlindner522@gmail.com>
2019-10-13 10:39:46 -0700

    Inject livereload-js when devtools is loaded and enabled

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,14 +1,43 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("#{systemProperties['java.class.path']}")
+    private String javaClassPath;
+
+    @Value("${spring.devtools.livereload.enabled:true}")
+    private boolean livereloadEnabled;
+
+    @Value("${spring.devtools.livereload.port:35729}")
+    private String livereloadPort;
+
+    @ResponseBody
     @RequestMapping("/")
-    public String getIndex(final Map<String, Object> model) {
-        return "index";
+    public String getIndex() {
+        Context context = new Context();
+        String html = templateEngine.process("index", context);
+
+        boolean devtoolsLoaded = javaClassPath.contains("spring-boot-devtools");
+        if (devtoolsLoaded && livereloadEnabled) {
+            html = addLiveReload(html);
+        }
+        return html;
+    }
+
+    private String addLiveReload(String html) {
+        html = html.replace("</body>", "<script src='/livereload.js?port=" + livereloadPort + "'></script>\n</body>");
+        return html;
     }
 }
