7f07b8f230dac70f9e1c14d0c31217d53d0f90b6
William Lindner <wlindner522@gmail.com>
2019-10-20 18:35:19 -0700

    Move webpack startup into the 'setup-frontend' plugin

--- buildSrc/src/main/java/cool/william/SetupFrontendPlugin.java
+++ buildSrc/src/main/java/cool/william/SetupFrontendPlugin.java
@@ -1,11 +1,37 @@
 package cool.william;
 
+import com.github.psxpaul.task.ExecFork;
+import groovy.lang.Closure;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 public class SetupFrontendPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        project.getTasks().create("setupReactFrontend", SetupFrontendTask.class);
+        project.getTasks()
+                .create("setupReactFrontend", SetupFrontendTask.class);
+
+        project.getPluginManager()
+                .apply("com.github.psxpaul.execfork");
+
+        ExecFork startWebpackWatch = project.getTasks()
+                .create("startWebpackWatch", ExecFork.class);
+        startWebpackWatch.setExecutable("node_modules/.bin/webpack");
+        startWebpackWatch.setArgs(Arrays.asList(new String[]{"--watch"}));
+        startWebpackWatch.setWaitForOutput("Built at");
+
+        Optional<Task> bootRun = project.getTasksByName("bootRun", true)
+                .stream()
+                .findFirst();
+        bootRun.ifPresent(startWebpackWatch::setStopAfter);
+
+        Optional<Task> processResources = project.getTasksByName("processResources", true)
+                .stream()
+                .findFirst();
+        processResources.ifPresent(task -> task.dependsOn(startWebpackWatch));
     }
 }
