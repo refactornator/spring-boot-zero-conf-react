ac1adcec5fb7cd1cef25b794bac69d3463d8ecf8
William Lindner <wlindner522@gmail.com>
2019-11-03 11:25:25 -0800

    Another way to import the IndexController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import cool.william.IndexController;
+import org.springframework.context.annotation.Import;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "cool.william")
+@SpringBootApplication
+@Import(IndexController.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
